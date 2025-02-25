ffee8536c9f7b3ad6b8bf18a9112c9b6e3701941
Dima Pustolyakov <dimaukropdred@gmail.com>
2025-02-25 12:31:28 +0200

    added 1 lab

---
+++ Lab 1.2/Lab 1.2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+
+        //string path = @"C:\Users\dimau\source\repos\Lab 1.2\output.json";
+
+
+        //Dictionary<string, List<string>> inputDict = new Dictionary<string, List<string>>()
+        //{
+        //    { "1", new List<string> { "A", "d" } },
+        //    { "2", new List<string> { "C", "B" } }
+        //};
+
+
+        //List<string> combinations = GenerateCombinations(inputDict);
+
+
+        //Console.WriteLine(string.Join(" ", combinations));
+
+
+        //SaveToJsonFile(combinations, path);
+
+
+        /////////////////////////////////////////////////////// TASK 3 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        List<string> A = new List<string> { "Elephant", "NewYork", "Ivory", "Apple", "Red", "Kite", "Umbrella" };
+
+
+        var result = A
+            .Select(s => s.First())   // Беремо перший символ кожного рядка
+            .Distinct()               // Видаляємо дублікати
+            .Reverse();               // Інвертуємо рядок
+
+        // Вивід результату
+        Console.WriteLine(string.Join("", result)); // Виведе "Ukraine"
+    }
+
+    static List<string> GenerateCombinations(Dictionary<string, List<string>> dict)
+    {
+        List<string> result = new List<string>();
+        GenerateCombinationsRecursive(dict, new List<string>(dict.Keys), 0, "", result);
+        return result;
+    }
+
+    static void GenerateCombinationsRecursive(
+        Dictionary<string, List<string>> dict,
+        List<string> keys,
+        int depth,
+        string currentCombination,
+        List<string> result)
+    {
+        
+        if (depth == keys.Count)
+        {
+           
+         
+           result.Add(currentCombination); // Якщо досягли глибини, додаємо комбінацію до результату
+            return; // не виходить з функції цілком, а повертає назад
+        }
+
+       
+        string key = keys[depth]; // який ключ ми зараз розглядаємо(спочатку keys[0] потім 1)
+       
+
+        
+        foreach (string letter in dict[key]) // Перебираємо всі літери з масиву літер 
+        {
+           
+            GenerateCombinationsRecursive(dict, keys, depth + 1, currentCombination + letter, result); // викликаємо для кожної літери ще одну рекурсію
+        }
+    }
+
+    static void SaveToJsonFile(List<string> combinations, string path)
+    {
+        try
+        {
+            
+            string json = JsonConvert.SerializeObject(combinations, Formatting.Indented); // Конвертуємо список у JSON
+
+            
+            File.WriteAllText(path, json); // Записуємо у файл
+            Console.WriteLine($"Результат збережено у файл за шляхом: {path}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка під час збереження JSON: {ex.Message}");
+        }
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,46 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+
+        List<int> list = new List<int>(); // cписок елементів
+        for (int i = 1; i <= 100; i++) // заповнити числами від 1 до 100
+        {
+            list.Add(i);
+        }
+
+
+        PrintListSpiral(list); 
+
+        
+
+
+    static void PrintListSpiral(List<int> list)
+        {
+            int itemsPerRow = 10; 
+            int totalRows = (int)Math.Ceiling((double)list.Count / itemsPerRow); // Загальна кількість рядків
+
+
+            for (int i = 0; i < totalRows; i++)
+            {
+                int startIndex = i * itemsPerRow; // 0 * 10=0; 1 * 10 = 10 etc.
+                int endIndex = Math.Min(startIndex + itemsPerRow, list.Count); //0 + 10 = 10 or 100(щоб не виходити за межі, якщо елементи не будуть ділитися націло totalRows)
+
+
+                List<int> row = list.GetRange(startIndex, endIndex - startIndex);
+
+                
+                if (i % 2 == 1)
+                {
+                    row.Reverse(); // Якщо це парний рядок, розвертаємо його
+                }
+
+                // Виводимо рядок
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+
+    
+};
